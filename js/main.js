2e685f736d895b493ed4ec8b2735927f903e8399
caiogomesxx <72234988+caiogomesxx@users.noreply.github.com>
2021-05-14 11:52:20 -0300

    a

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * @file Main runtime
+ * @author Gleidson Braga 
+ */
+
+ const configureFormEvents = () => {
+    const form = document.getElementsByTagName('form').item(0);
+    form.addEventListener('keydown', formOnChange);
+    form.addEventListener('submit', formOnSubmit);
+}
+
+window.onload = () => {
+    configureFormEvents();
+    renderJobPage();
+}
+function goBack() {
+    window.history.back();
+  }
